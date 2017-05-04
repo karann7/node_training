b2436b30e63fa917ffba1314e148fa1c2ce5f534
Karan Singh <kar.n@icloud.com>
2017-05-04 10:35:35 -0700

    deleted depencies and updated tests with assertion library

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -1,6 +1,25 @@
-"use strict";
+const expect = require('expect');
 const utils = require('./utils');
 
-it('should add two numbers', ()=>{
-	var res = utils.add(5,6);
+it('should add two numbers', () => {
+	var res = utils.add(33,11);
+
+	expect(res).toBe(44).toBeA('number');
+	// if(res !== 44){
+	// 	throw new Error(`Expected 44, but got ${res}`);
+	// }
+});
+
+it('should square the number', ()=>{
+	var sqrt = utils.square(6);
+
+	expect(sqrt).toBe(36).toBeA('number');
+	
+});
+
+it('expect a value', () => {
+	// expect(12).toNotBe(11);
+	//can not use toBe for array or Object comparsions have to use toEqual
+	// expect({name: "Karan"}).toEqual({name: "Karan"});
+	expect([2,3,4]).toExclude(5);
 });
