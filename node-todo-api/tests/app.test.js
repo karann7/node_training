0f2620a8cf2ed99b4997c8fddc89123b48b35a3c
Karan Singh <kar.n@icloud.com>
2017-05-05 16:43:18 -0700

    added a test case for the GET route also, redefined the test for the post route to be more accurate

--- node-todo-api/tests/app.test.js
+++ node-todo-api/tests/app.test.js
@@ -9,15 +9,13 @@ const expect 		= require('expect'),
 		}, {
 			text: "second todo"
 		}];
-//commented out so that the DB doesn't get wiped on running the test
+//Seeds the DB with exactly 2 todos before each test
  beforeEach((done)=>{
  	Todo.remove({}).then(()=> {
  		return Todo.insertMany(todos);
  	}).then(()=> done());
  });
- // beforeEach((donee)=>{
- // 	User.remove({}).then(()=> donee());
- // });
+
 describe('POST /todos', ()=>{
 	it('should create a new todo', (done)=>{
 		var text = "my name is karan";
@@ -69,45 +67,3 @@ describe('GET /todos', ()=>{
 		.end(done);
 	});
 });
-
-
-
-// // testing the POST user route
-// describe('POST /users', ()=>{
-// 	it('should create a new user', (donee)=>{
-// 		var email = "kar.n@icloud.com";
-
-// 		request(app)
-// 		.post('/users')
-// 		.send({email})
-// 		.expect(200)
-// 		.expect((res)=>{
-// 			expect(res.body.email).toBe(email);
-// 		})
-// 		.end((err, res) =>{
-// 			if(err){
-// 				return donee(err);
-// 			}
-// 			User.find().then((users)=>{
-// 				expect(users.length).toBe(1);
-// 				expect(users[0].email).toBe(email);
-// 				donee();
-// 			}).catch((e)=> donee(e));
-// 			});
-// 	});
-// 	it('should not create a user with invalid data', (donee)=>{
-// 		request(app)
-// 		.post('/users')
-// 		.send({})
-// 		.expect(400)
-// 		.end((err, res)=>{
-// 			if(err){
-// 				return donee(err);
-// 			}
-// 			User.find().then((users)=>{
-// 				expect(users.length).toBe(0);
-// 				donee();
-// 			}).catch((e)=> donee(e));
-// 		});
-// 	});
-// });
